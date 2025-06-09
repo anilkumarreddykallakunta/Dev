318910146ec74bec6ba8b3014c78ff9a0649f7b0
kAnil-reddy <anilreddy.k2266@gmail.com>
2025-06-09 15:04:17 +0530

    secondcommit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+
 app.use("/test",(req,res)=>{
-    res.send("from the big   server");
+    res.send("from the big big   server");
 });
 app.use("/hello",(req,res)=>{
-    res.send("Hello   server");
+    res.send("Hello theee   server");
+});
+app.use("/",(req,res)=>{
+    res.send("Hello   the server");
 })
 app.listen(7777, ()=>{
     console.log("server is succesfully on given port");
