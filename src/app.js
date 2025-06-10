47276bdf5846a336997663a2741cf82489a74d84
kAnil-reddy <anilreddy.k2266@gmail.com>
2025-06-10 20:24:36 +0530

    small changes in the routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+pp.use("/hello/2",(req,res)=>{
+    res.send("Hello theee   server");
 
+});
 app.use("/test",(req,res)=>{
     res.send("from the big big   server");
+    
 });
 app.use("/hello",(req,res)=>{
     res.send("Hello theee   server");
+
 });
 app.use("/",(req,res)=>{
     res.send("Hello   the server");
