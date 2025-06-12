4102b9ee2b22ab95bb3f5e941d017c850f7cd861
anilkumarreddykallakunta <anilreddy.k2266@gmail.com>
2025-06-12 10:56:55 +0530

    Changes Done

--- src/interviewproblem3.js
+++ src/interviewproblem3.js
@@ -1,4 +1,4 @@
-console.log(false || null || "Hello");
+console.log(false || null || "Hi");
 console.log(false && null && "Hello");
 // In first line it is OR operator is used it returns first truthy value.
 // In AND operator it will return first falsy value
