9b233fe4497863c4a351a74c1a181490416f6e2b
anilkumarreddykallakunta <anilreddy.k2266@gmail.com>
2025-06-12 11:42:29 +0530

    kept one array empty

--- src/interviewquestion4.js
+++ src/interviewquestion4.js
@@ -1,5 +1,5 @@
-var x = [2,6];
-var y = [1,4];
+var x = [1,6];
+var y = [];
 // It uses basically javascript cohersion concept
 // Internally it uses [].toString method and concat the two arrays 
 // '1' + '2' = '12'
