6b982b3b2314b965dfb79e872c400a88c8281f1b
anilkumarreddykallakunta <anilreddy.k2266@gmail.com>
2025-06-12 14:54:01 +0530

    Deloitte interview Question

---
+++ src/interviewquestion7.js
@@ -0,0 +1,4 @@
+const str = "abc" + + "def";
+console.log(str);
+// that def before + it will converted into number  but "def" doesn't convert string to number and abc is string. 
+// it will print abcNaN
