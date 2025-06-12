8f01753342845af9192d98003279d880d6e1619c
anilkumarreddykallakunta <anilreddy.k2266@gmail.com>
2025-06-12 11:49:51 +0530

    === checking

--- src/interviewquestion5.js
+++ src/interviewquestion5.js
@@ -1,4 +1,4 @@
 var x = [1,2]; // it uses type cohersion concept in javascript .
 //  It will use array[].tostring method it will convert to string 
 var y ='1,2';
-console.log(x==y);
+console.log(x===y);
