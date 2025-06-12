1608ce8d39636b4dded9a1dd210769fbcafbb90e
anilkumarreddykallakunta <anilreddy.k2266@gmail.com>
2025-06-12 11:41:33 +0530

    changes in the array

--- src/interviewquestion4.js
+++ src/interviewquestion4.js
@@ -1,5 +1,5 @@
-const x = [1,3,6];
-const y = [2,4];
+var x = [2,6];
+var y = [1,4];
 // It uses basically javascript cohersion concept
 // Internally it uses [].toString method and concat the two arrays 
 // '1' + '2' = '12'
