7b03b1ae590cc6a2d09bdc587506e9b11beb9fb5
anilkumarreddykallakunta <anilreddy.k2266@gmail.com>
2025-06-12 15:06:55 +0530

    capgemini intervew question

---
+++ src/interviewquestion8.js
@@ -0,0 +1,4 @@
+let newlist = [100].push(2); // when we  push an element to an array.It will
+//  return the length of an array.
+console.log(newlist);
+console.log(newlist.push(6));// It will throw an type error
